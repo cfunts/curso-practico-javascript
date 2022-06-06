226cd37a62f643b354482abde348773998a9d24c
cfunts <c.fuentes9824@gmail.com>
2022-06-06 15:39:58 -0500

    commit

--- taller_1_figuras_geometricas/figuras.js
+++ taller_1_figuras_geometricas/figuras.js
@@ -1,14 +1,19 @@
+//*******Funciones auxiliares para el cuadrado***********
 console.group("Cuadrados");
 function perimetroCuadrado(lado)
 {
     return lado*4;
 }
+
 function areaCuadrado(lado)
 {
     return lado*lado;
 }
 console.groupEnd();
 
+//***********************
+
+//*******Funciones auxiliares para el Triangulo***********
 console.group("Triángulos");
 function perimetroTriangulo(lado1,lado2,base)
 {
@@ -18,9 +23,10 @@ function areaTriangulo(base,altura)
 {
     return(base*altura)/2;
 }
-console.log("El área del triángulo es: "+areaTriangulo+"cmˆ2");
+
 console.groupEnd();
 
+//*******Funciones auxiliares para el circulo***********
 console.group("Círculos");
 function diametroCirculo(radio)
 {
@@ -38,6 +44,13 @@ function areaCirculo(radio)
 }
 console.groupEnd();
 
+
+
+
+
+
+//Interacción con el archivo HTML (los buttons)
+//------------------Cuadrado-----------------------
 function calcularPerimetroCuadrado()
 {
     const input=document.getElementById("InputCuadrado");
@@ -53,3 +66,54 @@ function calcularAreaCuadrado()
     const area=areaCuadrado(value);
     alert(area);
 }
+//------------------Triangulo-----------------------
+function calcularPerimetroTriangulo()
+{
+    let input=document.getElementById("InputTriangulo-1");
+    let value1=parseInt(input.value);
+    let input2=document.getElementById("InputTriangulo-2");
+    let value2=parseInt(input2.value);
+    let input3=document.getElementById("InputTriangulo-3");
+    let value3=parseInt(input3.value);
+    let perimetroT=perimetroTriangulo(value1,value2,value3);
+    alert(perimetroT);
+}
+
+function calcularAreaTriangulo()
+{
+
+    let input3=document.getElementById("InputTriangulo-3");
+    let value3=parseInt(input3.value);
+    let input4=document.getElementById("InputTriangulo-3");
+    let value4=parseInt(input4.value);
+    let areaT=areaTriangulo(value3,value4);
+    alert(areaT);
+    console.log("El área del triángulo es: "+areaT+"cmˆ2");
+}
+
+//----------------Circulo--------------------------
+function calcularDiametroCirculo()
+{
+    const input=document.getElementById("InputCirculo");
+    const value=input.value;
+    const diametro=diametroCirculo(value);
+    alert(diametro);
+}
+
+function calcularPerimetroCirculo()
+{
+    const input=document.getElementById("InputCirculo");
+    const value=input.value;
+    const perimetroC=perimetroCirculo(value);
+    alert(perimetroC);
+}
+
+function calcularAreaCirculo()
+{
+    const input=document.getElementById("InputCirculo");
+    const value=input.value;
+    const areaC=areaCirculo(value);
+    alert(areaC);
+}
+
+
